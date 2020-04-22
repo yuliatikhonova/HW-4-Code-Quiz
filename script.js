179da0f0f98c8f81909782f4b0bba775b355998c
yulia tikhonova <yuliatikhonova@outlook.com>
2020-04-21 20:56:49 -0700

    adjusted the html and js so when the start button is clicked the intro is hiden and the question and answer show up

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-var question = document.querySelector("#question");
-var answer = document.querySelector("#answer");
-var grade = document.querySelector("#grade")
+// var question = document.querySelector("#question");
+// var answer = document.querySelector("#answer");
+// var grade = document.querySelector("#grade")
 var startBtn = document.querySelector("#start-btn");
+var intro = document.querySelector("#intro")
+var questionContainer = document.querySelector("#question-container")
 var score = 0;
 var secondsLeft = 75;
 
-//-------------------------------------------------------------------
+//-------------------------------------------------------------------Start
 //clicking the start button
-startBtn.addEventListener("click", function() { 
-    startQuiz();
-});
+startBtn.addEventListener("click", startQuiz);
 
 //function when start button is pressed
 function startQuiz() {
     setTimer();
-
+    intro.classList.add("hide");
+    questionContainer.classList.remove("hide");
+    setNextQuestion()
 }
 
-//-------------------------------------------------------------------
+//-------------------------------------------------------------------Timer
 //timer function
 function setTimer() {
     var timerInterval = setInterval(function () {
@@ -26,20 +28,42 @@ function setTimer() {
 
         if (secondsLeft === 0) {
             clearInterval(timerInterval);
-            //function showScore
+            //function showScore and ask for initials
         }
     }, 1000);
 
 }
 
-//function to show the score and get initials
-//  function showScore(){}
+//------------------------------------------------------------------- Questions
 
-//keeping score function
-// function keepScore() {}
 
-//-------------------------------------------------------------------
-//function for the questions
+//Array for questions and answers
+
+//Loop for the array---------------------------
+//answer check. If correct (count ++; by 5 points) else (secondsLeft --; by 10 sec). Tie it to the showScore
+// for ( var i = 0; i < question.length; i++) {
+//     var response = window.___(question[i].___);
+//     if(response == question[i].answer){
+//         score++; 5pt
+//         grade.----("Correct")
+//     } else{
+//         secondsLeft--; 10sec
+//         grade.----("Incorrect")
+//     }
+// }
+
+//------------------------------------------------------------------- Show score and initials
+//function to show the score ask for initials
+// function showScore(){
+
+// }
+
+// submitting the initials
+// show score and initials
+
+
+
+
 
 
 
